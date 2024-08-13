06338711bb30b7235643d1c43b0a219e8371a61c
Angela Enriquez <angelaenriquezgar@gmail.com>
2024-08-13 11:05:09 +1000

    av

---
+++ 0_av.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - prints all command-line arguments
+ * @ac: argument count, parameter unused
+ * @av: argument vector
+ *
+ * Return: 0 on success
+ */
+
+int main(__attribute__((unused)) int ac, char **av)
+{
+	int i;
+
+	for (i = 0; av[i] != NULL; i++)
+	{
+		printf("%s\n", av[i]);
+	
+	}
+	return (0);
+}
